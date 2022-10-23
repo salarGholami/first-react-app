7ecedb65f59a7dc590d6d2b17b21003b02fd3d88
Salar <Salargholami021@gmail.com>
2022-10-23 03:50:49 +0330

    added App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div className="container" id="title">
+      <h1>shopping App</h1>
+      <p>product #1 : book 1</p>
+    </div>
+  );
+};
+
+export default App;
