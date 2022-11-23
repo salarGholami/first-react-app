d7f0e617d1f7dd73f2450b550f16e205397afba9
Salar <Salargholami021@gmail.com>
2022-11-23 06:18:04 +0330

    import ProductList to my component

--- src/App.js
+++ src/App.js
@@ -1,50 +1,17 @@
-import Product from "./comppnent/product/Product.js";
 import { Component } from "react";
 import "./App.css";
+import ProductList from "./comppnent/productList/ProductList.js";
 
 class App extends Component {
   state = {
-    products: [
-      { title: "React.js", price: "8$", id: 1 },
-      { title: "Next.js", price: "10$", id: 2 },
-      { title: "Node.js", price: "7$", id: 3 },
-    ],
-    count: 0,
-  };
-
-  countHandler(id) {
-    console.log("count click ", id);
-    this.setState({ count: this.state.count + 1 });
-  }
-
-  clickHandler = (newData) => {
-    this.setState({
-      products: [
-        { title: "React.js", price: "89$", id: 1 },
-        { title: newData, price: "109$", id: 2 },
-        { title: "Node.js", price: "7 9$", id: 3 },
-      ],
-    });
+    
   };
 
   render() {
     return (
       <div className="container" id="title">
         <h1>shopping App</h1>
-        {this.state.products.map((product) => {
-          return (
-            <Product
-              name={product.title}
-              price={product.price}
-              key={product.id}
-              click={()=>this.clickHandler("new DataClicked")}
-            />
-          );
-        })}
-        {/* <button onClick={this.clickHandler}>بده بزنیم</button> */}
-        <button onClick={this.countHandler.bind(this, 2)}>
-          counter:{this.state.count}
-        </button>
+        <ProductList />
       </div>
     );
   }
