2a50c01ae762b9b1c68edf107be4725f6d6672e2
Salar <Salargholami021@gmail.com>
2022-12-05 16:45:47 +0330

    feature/ add event handler for increment and decrement

--- src/comppnent/productList/ProductList.js
+++ src/comppnent/productList/ProductList.js
@@ -4,21 +4,57 @@ import Product from "../product/Product";
 class ProductList extends Component {
   state = {
     products: [
-      { title: "React.js", price: "8$", id: 1 },
-      { title: "Next.js", price: "10$", id: 2 },
-      { title: "Node.js", price: "7$", id: 3 },
+      { title: "React.js", price: "8$", id: 1, quantity: 1 },
+      { title: "Next.js", price: "10$", id: 2, quantity: 3 },
+      { title: "Node.js", price: "7$", id: 3, quantity: 2 },
     ],
   };
+
+  deleteHandler = (id) => {
+    console.log("delete", id);
+    const filterProduct = this.state.products.filter((p) => p.id !== id);
+    this.setState({ products: filterProduct });
+  };
+
+  incrementHandler = (id) => {
+    console.log("increment", id);
+    const product = [...this.state.products];
+    const selectedItem = product.find((p) => p.id === id);
+    selectedItem.quantity++;
+    this.setState({ products: product });
+  };
+
+  decrementHandler = (id) => {
+    console.log("decrement", id);
+    const product = [...this.state.products];
+    const selectedItem = product.find((p) => p.id === id);
+    if (selectedItem.quantity === 1) {
+      const filterProduct = this.state.products.filter((p) => p.id !== id);
+      this.setState({ products: filterProduct });
+    } else {
+      selectedItem.quantity--;
+      this.setState({ products: product });
+    }
+  };
+
+  changeHandler = (event, id) => {
+    const product = [...this.state.products];
+    const selectedItem = product.find((p) => p.id === id);
+    selectedItem.title = event.target.value;
+    this.setState({ products: product });
+  };
+
   render() {
     return (
       <div>
         {this.state.products.map((product) => {
           return (
             <Product
-              name={product.title}
-              price={product.price}
+              product={product}
               key={product.id}
-              click={() => this.clickHandler("new DataClicked")}
+              btnDelete={() => this.deleteHandler(product.id)}
+              btnIncrement={() => this.incrementHandler(product.id)}
+              btnDecrement={() => this.decrementHandler(product.id)}
             />
           );
         })}
