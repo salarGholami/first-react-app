5172342d33e464f5372783f0182bf1727e8a3cf6
Salar Gholami <salargholami021@gmail.com>
2022-12-08 18:04:25 +0330

    feature/ add trash icon with conditional rendreing

--- src/comppnent/product/Product.js
+++ src/comppnent/product/Product.js
@@ -1,4 +1,5 @@
 import styles from "./Product.module.css";
+import { BiTrash } from "react-icons/bi";
 
 const Product = (props) => {
   return (
@@ -6,9 +7,16 @@ const Product = (props) => {
       <p>product name : {props.product.title} course</p>
       <p>product price : {props.product.price}</p>
       <span className={styles.value}>{props.product.quantity}</span>
-      <input className={styles.input} type="text" onChange={props.onChange} value={props.product.title} />
+      <input
+        className={styles.input}
+        type="text"
+        onChange={props.onChange}
+        value={props.product.title}
+      />
       <button onClick={props.btnIncrement}>+</button>
-      <button onClick={props.btnDecrement}>-</button>
+      <button onClick={props.btnDecrement}>
+        {props.product.quantity > 1 ? "-" : <BiTrash />}
+      </button>
       <button onClick={props.btnDelete}>delete</button>
     </div>
   );
