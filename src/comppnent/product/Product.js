271b4428f6ab16d90441f838b91100ce49d0e987
Salar Gholami <salargholami021@gmail.com>
2022-12-08 17:50:44 +0330

    feature/ add value to input

--- src/comppnent/product/Product.js
+++ src/comppnent/product/Product.js
@@ -6,7 +6,7 @@ const Product = (props) => {
       <p>product name : {props.product.title} course</p>
       <p>product price : {props.product.price}</p>
       <span className={styles.value}>{props.product.quantity}</span>
-      <input className={styles.input} type="text" onChange={props.onChange} />
+      <input className={styles.input} type="text" onChange={props.onChange} value={props.product.title} />
       <button onClick={props.btnIncrement}>+</button>
       <button onClick={props.btnDecrement}>-</button>
       <button onClick={props.btnDelete}>delete</button>
