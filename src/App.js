87d92a5e1369f4bf621f52f3bd8bd417d29c10eb
Salar Gholami <salargholami021@gmail.com>
2022-12-10 16:11:04 +0330

    feature/add NavBar and import in App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import { Component } from "react";
 import "./App.css";
+import Navbar from "./comppnent/NavBar/NavBar";
 import ProductList from "./comppnent/productList/ProductList.js";
 
 class App extends Component {
-  state = {
-    
-  };
+  state = {};
 
   render() {
     return (
       <div className="container" id="title">
         <h1>shopping App</h1>
+        <Navbar />
         <ProductList />
       </div>
     );
---
+++ src/comppnent/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import style from "./NavBar.module.css";
+
+const Navbar = (props) => {
+  return (
+    <header className={style.navBar}>
+      <h2>gitarisma online shopping</h2>
+      <span>0</span>
+    </header>
+  );
+};
+
+export default Navbar;
