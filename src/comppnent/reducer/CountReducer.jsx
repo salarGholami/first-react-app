a73dee68e2e6c11be163e4618c51e61d0d4e3c16
Salar Gholami <salargholami021@gmail.com>
2023-02-05 06:16:47 +0330

    use multiple useReducer for state

--- src/comppnent/reducer/CountReducer.jsx
+++ src/comppnent/reducer/CountReducer.jsx
@@ -1,25 +1,16 @@
 import { useReducer } from "react";
 
-const initialState = {
-  firstCounter: 0,
-  secondCounter: 0,
-};
+const initialState = 0;
 
 const reducer = (state, action) => {
   console.log(state, action);
   switch (action.type) {
     case "addOne":
-      return { ...state, firstCounter: state.firstCounter + action.value };
+      return state + action.value;
     case "addFive":
-      return { ...state, firstCounter: state.firstCounter + action.value };
+      return state + action.value;
     case "decrement":
-      return { ...state, firstCounter: state.firstCounter - action.value };
-    case "addOne2":
-      return { ...state, secondCounter: state.secondCounter + action.value };
-    case "addFive2":
-      return { ...state, secondCounter: state.secondCounter + action.value };
-    case "decrement2":
-      return { ...state, secondCounter: state.secondCounter - action.value };
+      return state - action.value;
     case "reset":
       return initialState;
     default:
@@ -29,14 +20,14 @@ const reducer = (state, action) => {
 
 const CounterReducer = () => {
   const [count, dipatch] = useReducer(reducer, initialState);
+  const [countTwo, twodipatch] = useReducer(reducer, initialState);
   return (
     <div>
-      <h2>count is one : {count.firstCounter}</h2>
-      <h2>count is two : {count.secondCounter}</h2>
+      <h2>count is one : {count}</h2>
       <div>
         <button onClick={() => dipatch({ type: "addOne", value: 1 })}>
           add one
-        </button>   
+        </button>
         <button onClick={() => dipatch({ type: "addFive", value: 5 })}>
           add five
         </button>
@@ -44,15 +35,16 @@ const CounterReducer = () => {
           decrement
         </button>
       </div>
+      <h2>count is two : {countTwo}</h2>
       <div>
-        <button onClick={() => dipatch({ type: "addOne2", value: 1 })}>
-          add one2
+        <button onClick={() => twodipatch({ type: "addOne", value: 1 })}>
+          add one
         </button>
-        <button onClick={() => dipatch({ type: "addFive2", value: 5 })}>
-          add five2
+        <button onClick={() => twodipatch({ type: "addFive", value: 5 })}>
+          add five
         </button>
-        <button onClick={() => dipatch({ type: "decrement2", value: 1 })}>
-          decrement2
+        <button onClick={() => twodipatch({ type: "decrement", value: 1 })}>
+          decrement
         </button>
       </div>
       <button onClick={() => dipatch({ type: "reset" })}>Reset</button>
